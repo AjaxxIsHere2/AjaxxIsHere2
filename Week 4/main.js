99eccbe42eb17fba610f6d35c2fe55418a8e55d4
AJ <mm2270@hw.ac.uk>
2023-11-03 09:38:08 +0400

    Update main.js

--- Week 4/main.js
+++ Week 4/main.js
@@ -1,27 +1,35 @@
-var myCity = {
-  name: "Dubai",
-  country: "UAE",
-  place: "Dubai Mall",
+// var myCity = {
+//   name: "Dubai",
+//   country: "UAE",
+//   place: "Dubai Mall",
+// };
+
+// console.log(myCity.name); // Outputs: Dubai
+// console.log(myCity.country); // Outputs: UAE
+
+// var myCity = ["Dubai", "UAE", "Dubai Mall"];
+
+// console.log(myCity[0]); // Outputs: Dubai
+
+// var myCities = [
+//   {
+//     name: "Dubai",
+//     country: "UAE",
+//     place: "Dubai Mall",
+//   },
+//   {
+//     name: "Milan",
+//     country: "Italy",
+//     place: "Duomo Di Milano",
+//   },
+// ];
+
+// console.log(myCities[1].country); // Outputs: Italy
+
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://AjaxxIsHere2.github.io/AjaxxIsHere2/city.json');
+ourRequest.onload = function() {
+console.log(ourRequest.responseText);
 };
-
-console.log(myCity.name); // Outputs: Dubai
-console.log(myCity.country); // Outputs: UAE
-
-var myCity = ["Dubai", "UAE", "Dubai Mall"];
-
-console.log(myCity[0]); // Outputs: Dubai
-
-var myCities = [
-  {
-    name: "Dubai",
-    country: "UAE",
-    place: "Dubai Mall",
-  },
-  {
-    name: "Milan",
-    country: "Italy",
-    place: "Duomo Di Milano",
-  },
-];
-
-console.log(myCities[1].country); // Outputs: Italy
+ourRequest.send();
